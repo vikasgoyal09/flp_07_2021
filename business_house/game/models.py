1c1d314a6c4371c49e750e8efb16a921a88f027c
zainshaikh11 <mohammad.shaikh@quovantis.com>
2021-07-11 14:31:28 +0530

    created GamePlayer & GameBoard models

--- business_house/game/models.py
+++ business_house/game/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+# Create your models here
+
+class GameBoard(models.Model):
+    max_players = models.IntegerField()
+    no_of_players = models.IntegerField(default=0)
+    bank_amount = models.IntegerField(default=0)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    cell_conf = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
+
+class GamePlayer(models.Model):
+    game_id = models.ForeignKey(GameBoard, on_delete=CASCADE)
+    user_id = models.ForeignKey(User, on_delete=CASCADE)
+    scored_credit = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "game_id: {} - user_id: {}".format(self.game_id, self.user_id)
